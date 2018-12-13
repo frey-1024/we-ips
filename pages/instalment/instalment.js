3113f13b6d70af42dedabb9342efebda24357cab
zhangkun <1766597067@qq.com>
2018-12-13 13:43:35 +0800

    利息申请

--- pages/instalment/instalment.js
+++ pages/instalment/instalment.js
@@ -1,3 +1,4 @@
+var stringUtil = require('../../utils/stringUtil.js');
 const app = getApp();
 
 Page({
@@ -6,74 +7,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    show: false,
+    phoneInfo: app.globalData.phoneInfo,
     isShowPhoneWarning: false,
-    test: '',
   },
-  decodePhoneNumber(encryptedData, iv) {
-    wx.request({
-      url: app.globalData.base_url + '/users/phone',
-      data:{
-        encryptedData: encryptedData,
-        iv: iv
-      },
-      header:{
-        "Content-Type":"applciation/json",
-        "sessionid": app.globalData.sessionid
-      },
-      method:"POST",
-      success:function(res){
-        app.globalData.phoneNumber = res.data.data.phoneNumber;
-        console.log(res);
-        wx.navigateTo({
-          url: '../applyDesign/applyDesign'
-        });
-      },
-      fail:function(err){},//请求失败
-      complete:function(){}//请求完成后执行的函数
-    })
-  },
-  getPhoneNumber(e) {
-    this.setData({
-      test: JSON.stringify(e)
-    });
-    var that = this;
-    if (e.detail.errMsg.indexOf(':fail') > -1) {
-      that.setData({
-        isShowPhoneWarning: true
-      });
-    } else {
-      if (!app.globalData.sessionid) {
-        app.userSessionIdReadyCallback = res => {
-          that.decodePhoneNumber(e.detail.encryptedData, e.detail.iv);
-        }
-      } else {
-        that.decodePhoneNumber(e.detail.encryptedData, e.detail.iv);
-      }
-    }
-  },
-  hideModal() {
-    this.setData({
-      test: ''
-    });
-    this.setData({
-      isShowPhoneWarning: false
-    });
-  },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     wx.setNavigationBarTitle({
       title: '0利息分期'
-    });
-    wx.checkSession({
-      success: function () {
-      },
-      fail:function(err){
-        app.wxLogin();
-      },
     });
   },
 
@@ -84,13 +26,83 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  goApply() {
+    var that = this;
+    wx.showLoading({
+      title: '加载中...',
+    });
+    wx.request({
+      url: app.globalData.base_url + '/loan/apply',
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "sessionid": app.globalData.sessionid
+      },
+      method: "POST",
+      data: {
+        contact: app.globalData.userInfo.nickName,
+        phone: app.globalData.phoneInfo.phoneNumber,
+      },
+      success: function(res){
+        wx.hideLoading();
+        console.log(res);
+        var data = res.data;
+        var aipStatus = stringUtil.apiError(app, data.code, '申请失败， 请重试');
+        if (!aipStatus) {
+          return;
+        }
+        wx.showToast({
+          title: '申请成功',
+          icon: 'success',
+          duration: 3000
+        });
+      },
+      fail: function(err){
+        wx.hideLoading();
+        wx.showToast({
+          title: '申请失败， 请重试',
+          icon: 'none',
+          duration: 3000
+        });
+      }
+    });
   },
-
+  getPhoneNumber(e) {
+    var that = this;
+    if (e.detail.errMsg.indexOf(':fail') > -1) {
+      that.setData({
+        isShowPhoneWarning: true
+      });
+    } else {
+      app.getPhoneInfo(e.detail, function(phoneInfo) {
+        app.globalData.phoneInfo = phoneInfo;
+        that.goApply();
+      });
+    }
+  },
+  hidePhoneModal() {
+    this.setData({
+      isShowPhoneWarning: false
+    });
+  },
+  onShow: function () {
+    if (app.globalData.phoneInfo) {
+      this.setData({
+        phoneInfo: app.globalData.phoneInfo,
+      });
+    }
+    app.userPhoneReadyCallback = () => {
+      if (app.globalData.phoneInfo) {
+        this.setData({
+          phoneInfo: app.globalData.phoneInfo,
+        });
+      }
+    };
+    app.removePhoneInfoCallback = () => {
+      this.setData({
+        phoneInfo: null,
+      });
+    };
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
